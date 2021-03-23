01f6172fd52746572cdc498e6f33a1ef9191c63c
thmaan <chucrute_thomas@hotmail.com>
2021-03-23 19:58:07 -0300

    adicionando delay

--- app/src/main/java/com/example/servidoralgoritmodecristian/Servidor.java
+++ app/src/main/java/com/example/servidoralgoritmodecristian/Servidor.java
@@ -1,5 +1,7 @@
 package com.example.servidoralgoritmodecristian;
 
+import android.util.Log;
+
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
@@ -27,9 +29,11 @@ public class Servidor extends Thread {
 
                 DataInputStream in = new DataInputStream(server.getInputStream());
                 long t0 = in.readLong();
+                Log.d("t0",""+t0);
                 long t1 = System.currentTimeMillis();
 
                 DataOutputStream out = new DataOutputStream(server.getOutputStream());
+
                 long t2 = System.currentTimeMillis();
 
                 out.writeLong(t0);      //Envia o tempo do proprio cliente
@@ -45,5 +49,13 @@ public class Servidor extends Thread {
                 break;
             }
         }
+
+    }
+    public long random(){
+        long max = 10000;
+        long min = 5000;
+        double random = (Math.random()*((max-min)+1))+min;
+        System.out.println("Random gerado: "+ (long) random);
+        return (long) random;
     }
 }
